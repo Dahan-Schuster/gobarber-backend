64c85c4ca3f15524d2f8cebc3d424fa6821267a7
Dahan Schuster <dan.plschuster@gmail.com>
2020-05-23 16:29:22 -0300

    Cria a rota de atualização de avatar do usuário
    Cria a rota PATCH /users/avatar, que recebe um arquivo e utiliza o
    multer como middleware para realizar o upload e o serviço de atualização
    de avatar para atualizar os dados na base

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,13 @@
 import { Router } from 'express';
+import multer from 'multer';
+
 import CreateUserService from '../services/CreateUserService';
+import UpdateUserAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadConfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadConfig);
 
 /**
  * Handle POST Users requests
@@ -23,5 +29,33 @@ usersRouter.post('/', async (req, res) => {
 		return res.status(400).json({ error: err.message });
 	}
 });
+
+/**
+ * Handle PATCH Users' avatars requests
+ * Upload a file to the system and save its path in the database
+ *
+ * @bodyParam avatar
+ *
+ * @since 1.0.0
+ */
+usersRouter.patch(
+	'/avatar',
+	ensureAuthenticated,
+	upload.single('avatar'),
+	async (req, res) => {
+		try {
+			const updateUserAvatarService = new UpdateUserAvatarService();
+
+			const user = await updateUserAvatarService.execute({
+				userId: req.user.id,
+				avatarFilename: req.file.filename,
+			});
+
+			return res.json(user);
+		} catch (err) {
+			return res.status(400).json({ error: err.message });
+		}
+	},
+);
 
 export default usersRouter;
