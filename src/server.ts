00434df5ad691b30f0f697373e55631ac06685ba
Dahan Schuster <dan.plschuster@gmail.com>
2020-06-04 22:47:31 -0300

    Instala o cors para controle das requisições
    Instala o pacote cors, responsável por controlar quem pode acessar a API
    via navegador. Este pacote irá permitir que o site feito em React possa
    acessar a API

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express, { NextFunction, Request, Response } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 
 import routes from './routes/index';
 import 'reflect-metadata';
@@ -10,6 +11,7 @@ import AppError from './errors/AppError';
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
