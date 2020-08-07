0797a9fef0cc20adf661631f5300d6a6cd7f5038
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-07 00:06:13 -0300

    Cria o container de dependências com o tsyringe
    Substitui a injeção manual das dependências dos services de agendamentos
    pela a resolução automática feita pelo Dependency Container

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -6,8 +6,8 @@
 
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { container } from 'tsyringe';
 
-import AppointmentsRepository from '@modules/appointments/infra/typeorm/repositories/AppointmentsRepository';
 import CreateAppointmentService from '@modules/appointments/services/CreateAppointmentService';
 
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
@@ -36,8 +36,8 @@ appointmentsRouter.post('/', async (req, res) => {
 	const { providerId, date } = req.body;
 	const parsedDate = parseISO(date); // data transformation, route responsibility
 
-	const createAppointmentService = new CreateAppointmentService(
-		new AppointmentsRepository(),
+	const createAppointmentService = container.resolve(
+		CreateAppointmentService,
 	);
 	const appointment = await createAppointmentService.execute({
 		providerId,
