ff57e3a338b598dda3847d09f7618a59ebe2faec
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-06 23:20:03 -0300

    Liskov Substitution Principle - Users
    Cria uma implementação da interface de repositório de usuário, que será
    utilizada de forma abstrata pela camada de Serviços

---
+++ src/modules/users/dtos/ICreateUserDTO.ts
@@ -0,0 +1,12 @@
+/**
+ * Interface ICreateUserDTO
+ * Interface for creating users
+ *
+ * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
+ * @version 1.0.0
+ */
+export default interface ICreateUserDTO {
+	name: string;
+	email: string;
+	password: string;
+}
---
+++ src/modules/users/infra/typeorm/repositories/UsersRepository.ts
@@ -0,0 +1,86 @@
+import { EntityRepository, getRepository, Repository } from 'typeorm';
+import User from '@modules/users/infra/typeorm/entities/User';
+import IUsersRepository from '@modules/users/infra/repositories/IUsersRepository';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+/**
+ * Class UsersRepository
+ * For operations with the users repository
+ *
+ * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
+ * @version 1.0.0
+ */
+@EntityRepository(User)
+class UsersRepository implements IUsersRepository {
+	private ormRepository: Repository<User>;
+
+	/**
+	 * UsersRepository's constructor
+	 *
+	 * @since 1.0.0 - Initial version
+	 */
+	constructor() {
+		this.ormRepository = getRepository(User);
+	}
+
+	/**
+	 * Finds an user by its UUID
+	 *
+	 * @param id
+	 * @return Promise<User | null>
+	 *
+	 * @since 1.0.0 - Initial version
+	 */
+	public async findById(id: string): Promise<User | undefined> {
+		const findUser = await this.ormRepository.findOne(id);
+		return findUser || undefined;
+	}
+
+	/**
+	 * Finds an user by its email
+	 *
+	 * @param email
+	 * @return Promise<User | null>
+	 *
+	 * @since 1.0.0 - Initial version
+	 */
+	public async findByEmail(email: string): Promise<User | undefined> {
+		const findUser = await this.ormRepository.findOne({
+			where: { email },
+		});
+
+		return findUser || undefined;
+	}
+
+	/**
+	 * Creates and saves an user in the database
+	 *
+	 * @param data: ICreateUserDTO
+	 * @return Promise<User | null>
+	 *
+	 * @since 1.0.0 - Initial version
+	 */
+	public async create({
+		name,
+		email,
+		password,
+	}: ICreateUserDTO): Promise<User> {
+		const user = this.ormRepository.create({ name, email, password });
+		await this.ormRepository.save(user);
+		return user;
+	}
+
+	/**
+	 * Saves an user's data in the database
+	 *
+	 * @param user
+	 * @return Promise<User | null>
+	 *
+	 * @since 1.0.0 - Initial version
+	 */
+	save(user: User): Promise<User> {
+		return this.ormRepository.save(user);
+	}
+}
+
+export default UsersRepository;
