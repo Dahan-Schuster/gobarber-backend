48c00882f792f5379e01e34d85a9d1b5cb81ab28
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-06 23:18:59 -0300

    Liskov Substitution Principle - Users
    Cria a interface que será utilizada para adicionar um nível à abstração
    entre as camadas de Infraestrutura e de Serviços na manipulação de dados
    de usuários

---
+++ src/modules/users/infra/repositories/IUsersRepository.ts
@@ -0,0 +1,18 @@
+import User from '@modules/users/infra/typeorm/entities/User';
+import ICreateUserDTO from '@modules/users/dtos/ICreateUserDTO';
+
+/**
+ * Interface IUsersRepository
+ *
+ * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
+ * @version 1.0.0
+ */
+export default interface IUsersRepository {
+	findById(id: string): Promise<User | undefined>;
+
+	findByEmail(email: string): Promise<User | undefined>;
+
+	create(data: ICreateUserDTO): Promise<User>;
+
+	save(user: User): Promise<User>;
+}
