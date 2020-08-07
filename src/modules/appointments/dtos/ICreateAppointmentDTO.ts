2024eab20fdb46152c2e5cf75d4ab902b343859e
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-06 23:13:57 -0300

    Liskov Substitution Principle - Appointments
    Modifica o repositório de appointments para implementar a interface
    criada, adicionando um nível de abstração na comunicação entre as
    camadas de infraestrutura e de serviços

---
+++ src/modules/appointments/dtos/ICreateAppointmentDTO.ts
@@ -0,0 +1,10 @@
+/**
+ * Interface ICreateAppointmentDTO
+ *
+ * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
+ * @version 1.0.0
+ */
+export default interface ICreateAppointmentDTO {
+	providerId: string;
+	date: Date;
+}
--- src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
+++ src/modules/appointments/infra/typeorm/repositories/AppointmentsRepository.ts
@@ -1,15 +1,28 @@
-import { EntityRepository, Repository } from 'typeorm';
+import { EntityRepository, getRepository, Repository } from 'typeorm';
 import Appointment from '@modules/appointments/infra/typeorm/entities/Appointment';
+import IAppointmentsRepository from '@modules/appointments/infra/repositories/IAppointmentsRepository';
+import ICreateAppointmentDTO from '@modules/appointments/dtos/ICreateAppointmentDTO';
 
 /**
  * Class AppointmentsRepository
  * For operations with the appointments repository
  *
  * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
- * @version 2.0.0 - Applies TypeOrm
+ * @version 2.1.0 - Implements own CRUD methods (removes inheritance from the typeorm Repository class)
  */
 @EntityRepository(Appointment)
-class AppointmentsRepository extends Repository<Appointment> {
+class AppointmentsRepository implements IAppointmentsRepository {
+	private ormRepository: Repository<Appointment>;
+
+	/**
+	 * AppointmentsRepository's constructor
+	 *
+	 * @since 2.1.0 - Initial version
+	 */
+	constructor() {
+		this.ormRepository = getRepository(Appointment);
+	}
+
 	/**
 	 * Finds an appointment by its book date
 	 * Returns null if no appointment is find
@@ -17,15 +30,32 @@ class AppointmentsRepository extends Repository<Appointment> {
 	 * @param date
 	 * @return Promise<Appointment | null>
 	 *
-	 * @since 1.0.0
+	 * @since 1.0.0 - Initial version
 	 * @since 2.0.0 - Use methods of TypeOrm to search in the repository
 	 */
-	public async findByDate(date: Date): Promise<Appointment | null> {
-		const findAppointment = await this.findOne({
+	public async findByDate(date: Date): Promise<Appointment | undefined> {
+		const findAppointment = await this.ormRepository.findOne({
 			where: { date },
 		});
 
-		return findAppointment || null;
+		return findAppointment || undefined;
+	}
+
+	/**
+	 * Creates and saves an appointment in the database
+	 *
+	 * @param data: ICreateAppointmentDTO
+	 * @return Promise<Appointment | null>
+	 *
+	 * @since 2.1.0 - Initial version
+	 */
+	public async create({
+		providerId,
+		date,
+	}: ICreateAppointmentDTO): Promise<Appointment> {
+		const appointment = this.ormRepository.create({ providerId, date });
+		await this.ormRepository.save(appointment);
+		return appointment;
 	}
 }
 
