3ab36d0ec056f7dbebb30e1d2337a41a9e32d7a9
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-22 11:44:56 -0300

    Implementação de template de emails
    Atualiza o EtherealMailProvider para receber um ISendMailDTO e
    configurar o envio de email a partir dele
    Também configura o provider para receber via injeção de dependências o
    provider de template de emails

--- src/shared/providers/MailProvider/implementations/EtherealMailProvider.ts
+++ src/shared/providers/MailProvider/implementations/EtherealMailProvider.ts
@@ -1,5 +1,9 @@
+import { inject, injectable } from 'tsyringe';
 import nodemailer, { Transporter } from 'nodemailer';
+
 import IMailProvider from '@shared/providers/MailProvider/models/IMailProvider';
+import ISendMailDTO from '@shared/providers/MailProvider/dtos/ISendMailDTO';
+import IMailTemplateProvider from '@shared/providers/MailTemplateProvider/models/IMailTemplateProvider';
 
 /**
  * Class FakeMailProvider
@@ -7,10 +11,14 @@ import IMailProvider from '@shared/providers/MailProvider/models/IMailProvider';
  * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
  * @version 1.0.0
  */
+@injectable()
 export default class EtherealMailProvider implements IMailProvider {
 	private client: Transporter;
 
-	constructor() {
+	constructor(
+		@inject('MailTemplateProvider')
+		private mailTemplateProvider: IMailTemplateProvider,
+	) {
 		nodemailer.createTestAccount().then(account => {
 			this.client = nodemailer.createTransport({
 				host: account.smtp.host,
@@ -24,12 +32,23 @@ export default class EtherealMailProvider implements IMailProvider {
 		});
 	}
 
-	public async sendMail(to: string, body: string): Promise<void> {
+	public async sendMail({
+		to,
+		from,
+		subject,
+		templateData,
+	}: ISendMailDTO): Promise<void> {
 		const info = await this.client.sendMail({
-			from: 'Equipe Gobarber <equipe@gobarber.com.br>',
-			to,
-			subject: 'Recuperação de senha',
-			text: body,
+			from: {
+				name: from?.name || 'Equipe Gobarber',
+				address: from?.email || 'equipe@gobarber.com.br',
+			},
+			to: {
+				name: to.name,
+				address: to.email,
+			},
+			subject,
+			html: await this.mailTemplateProvider.parse(templateData),
 		});
 
 		console.log('Message sent');
