dc516e83245a9d98360f233431705f0177a23e02
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-23 12:59:32 -0300

    Implementação de agendamento de serviços
    Cria o controlador de listagem de provedores de serviços

---
+++ src/modules/appointments/infra/http/controllers/ProvidersController.ts
@@ -0,0 +1,35 @@
+import { Request, Response } from 'express';
+import { container } from 'tsyringe';
+
+import ListProvidersService from '@modules/appointments/services/ListProvidersService';
+
+/**
+ * Class AppointmentController
+ *
+ * @author Dahan Schuster <dan.plschuster@gmail.com> <github:dahan-schuster>
+ * @version 1.0.0
+ */
+export default class ProvidersController {
+	/**
+	 * Calls the Lis to ListProvidersService to list all providers
+	 *
+	 * @param request - an array of user IDs can be passed to exclude from the list
+	 * @param response
+	 * @return Promise<Response>
+	 *
+	 * @since 1.0.0
+	 */
+	public async index(
+		request: Request,
+		response: Response,
+	): Promise<Response> {
+		const { id: userId } = request.user;
+
+		const listProvidersService = container.resolve(ListProvidersService);
+		const users = await listProvidersService.execute({
+			exceptUserIds: [userId],
+		});
+
+		return response.json(users);
+	}
+}
