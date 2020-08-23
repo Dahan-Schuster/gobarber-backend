587d6a402f65c2554c9c201c79ef00f8f8d8c4f0
Dahan Schuster <dan.plschuster@gmail.com>
2020-08-23 17:22:42 -0300

    Implementação de validação das rotas
    Aplica a validação das rotas de agendamentos

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -1,10 +1,5 @@
-/**
- * Appointments Routes
- * @author Dahan Schuster <dan.plschuster@gmail.com>
- * @version 3.2.0 - Applies the use of the controllers
- */
-
 import { Router } from 'express';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 import AppointmentsController from '@modules/appointments/infra/http/controllers/AppointmentsController';
 import ProviderAppointmentsController from '@modules/appointments/infra/http/controllers/ProviderAppointmentsController';
@@ -17,24 +12,16 @@ const providerAppointmentsController = new ProviderAppointmentsController();
 
 appointmentsRouter.use(ensureAuthenticated);
 
-/**
- * Handle POST Appointments requests
- * Receives the request, instantiate and calls
- * the createAppointmentService, and then sends the response
- *
- * @bodyParam providerId
- * @bodyParam date
- *
- * @since 1.0.0
- * @since 1.1.0 - Applies the Single Responsibility Principle, from SOLID. This version uses the service to create the
- * new appointment, giving to the router the unique work of receive a request, call a file and send a response
- * @since 1.2.0 - Updates the use of the createAppointmentService after the changes with TypeOrm
- * @since 2.0.0 - Change the provider body param to providerId
- * @since 2.1.0 - Removes try/catch block because there's a Global Exception Handler
- * @since 3.0.0 - Applies Liskov Substitution Principle
- * @since 3.1.0 - Moves the route's logic to the controller
- */
-appointmentsRouter.post('/', appointmentsController.create);
+appointmentsRouter.post(
+	'/',
+	celebrate({
+		[Segments.BODY]: {
+			providerId: Joi.string().uuid().required(),
+			date: Joi.date().required(),
+		},
+	}),
+	appointmentsController.create,
+);
 
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
--- src/modules/appointments/infra/http/routes/providers.routes.ts
+++ src/modules/appointments/infra/http/routes/providers.routes.ts
@@ -3,6 +3,7 @@ import { Router } from 'express';
 import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 import ProvidersController from '@modules/appointments/infra/http/controllers/ProvidersController';
 import ProvidersAvailabilityController from '@modules/appointments/infra/http/controllers/ProvidersAvailabilityController';
+import { celebrate, Joi, Segments } from 'celebrate';
 
 const providersRouter = Router();
 const providersController = new ProvidersController();
@@ -13,10 +14,20 @@ providersRouter.use(ensureAuthenticated);
 providersRouter.get('/', providersController.index);
 providersRouter.get(
 	'/:id/month-availability',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
 	providersAvailabilityController.monthAvailability,
 );
 providersRouter.get(
 	'/:id/day-availability',
+	celebrate({
+		[Segments.PARAMS]: {
+			id: Joi.string().uuid().required(),
+		},
+	}),
 	providersAvailabilityController.dayAvailability,
 );
 
